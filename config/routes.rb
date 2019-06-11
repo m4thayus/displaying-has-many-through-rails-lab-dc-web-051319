bddfbd6c821e722c4945ddb5b6709e5e8d81893f
Matt Williams <matt.seongjun@gmail.com>
2019-06-11 16:07:33 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'appointment/show'
-
-  get 'patient/index'
-
-  get 'patient/show'
-
-  get 'doctor/index'
-
-  get 'doctor/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
